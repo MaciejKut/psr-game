baf4959a7deaf10626f2ca0490b6f244ba026ef0
MaciejKut <maciej.kutzmann@gmail.com>
2019-01-19 00:54:49 +0100

    Add script.js updates etap 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 window.onload = function () {
-    var scissors = document.getElementById('scissors');
-    var paper = document.getElementById('paper');
-    var rock = document.getElementById('rock');
+
     var results = document.getElementById('output')
     var wyniki = document.getElementById('wyniki');
     var newGame = document.getElementById('newGame');
@@ -11,49 +9,50 @@ window.onload = function () {
     var playerResult = document.getElementById('userResult')
     var computerResult = document.getElementById('compResult');
     var drawResult = document.getElementById('drawResult');
-    var roundcounter = 0;
-    var human = "human";
-    var bot = "bot";
-    var draw = "draw";
-    var manDraw;
-    var botDraw;
-    var manWins = 0;
-    var botWins = 0;
-    var remisy = 0;
-    var user;
+    var params = {
+        'roundcounter': 0,
+        'manWins': 0,
+        'botWins': 0,
+        'remisy': 0,
+        'user': '',
+        'manDraw': 0,
+        'botDraw': 0
+    };
+
 
     newGame.addEventListener('click', function () {
-        user = window.prompt('Podaj imię', 'Stranger');
-        console.log(user);
-        userPlayCallback();
-        roundcounter = 0;
-        manWins = 0;
-        botWins = 0;
-        remisy = 0;
+        params.user = window.prompt('Podaj imię', 'Stranger');
+        params.roundcounter = 0;
+        params.manWins = 0;
+        params.botWins = 0;
+        params.remisy = 0;
         results.innerHTML = ' ';
-        welcome.innerHTML = '<h1>Cześć ' + user + '!</h1>';
+        welcome.innerHTML = '<h1>Cześć ' + params.user + '!</h1>';
         wyniki.classList.remove('nonVisible');
         wyniki.classList.add('visible');
         mainDiv1.classList.add('visible');
         mainDiv1.classList.remove('nonVisible');
         mainDiv2.classList.add('nonVisible');
-        console.log(mainDiv2.classList);
-        console.log(mainDiv1.classList);
         updateResults();
-
-
     });
 
 
     function computerDraw() {
         var example = Math.floor((Math.random() * 3) + 1);
+        if (example == 1) {
+            example = 'paper';
+        } else if (example == 2) {
+            example = 'scissors';
+        } else {
+            example = 'rock';
+        }
         return example;
     }
 
-    function convertNumber(x) {
-        if (x == 1) {
+    function translate(x) {
+        if (x == 'paper') {
             return 'Papier';
-        } else if (x == 2) {
+        } else if (x == 'scissors') {
             return 'Nożyczki';
         } else {
             return 'Kamień';
@@ -61,36 +60,38 @@ window.onload = function () {
     };
 
     function whoWins(x, y) {
-        if ((x == 1 && y == 3) || (x == 2 && y == 1) || (x == 3 && y == 2)) {
-            manWins++
-            return human;
+        if ((x == 'paper' && y == 'rock') || (x == 'scissors' && y == 'paper') || (x == 'rock' && y == 'scissors')) {
+            params.manWins++
+            return 'human';
         } else if (x === y) {
-            remisy++;
-            return draw;
+            params.remisy++;
+            return 'draw';
 
         } else {
-            botWins++
-            return bot;
+            params.botWins++
+            return 'bot';
         }
 
     }
 
     function updateResults() {
-        playerResult.innerHTML = manWins;
-        computerResult.innerHTML = botWins;
-        drawResult.innerHTML = remisy;
+        playerResult.innerHTML = params.manWins;
+        computerResult.innerHTML = params.botWins;
+        drawResult.innerHTML = params.remisy;
     }
 
     var callbackOutput = function (x) {
+        console.log('params.botDraw ' + params.botDraw);
+        console.log('params.manDraw ' + params.manDraw);
 
         if (x == "human") {
 
-            results.innerHTML = '<tr><th scope="row">' + roundcounter + '</th><td>' + convertNumber(manDraw) + '</td><td>' + convertNumber(botDraw) + '</td><td>Wygrywa  ' + user + '</td></tr >';
+            results.innerHTML = '<tr><th scope="row">' + params.roundcounter + '</th><td>' + translate(params.manDraw) + '</td><td>' + translate(params.botDraw) + '</td><td>Wygrywa  ' + params.user + '</td></tr >';
         } else if (x == "draw") {
-            results.innerHTML = '<tr><th scope="row">' + roundcounter + '</th><td>' + convertNumber(manDraw) + '</td><td>' + convertNumber(botDraw) + '</td><td>Remis!</td></tr >';
+            results.innerHTML = '<tr><th scope="row">' + params.roundcounter + '</th><td>' + translate(params.manDraw) + '</td><td>' + translate(params.botDraw) + '</td><td>Remis!</td></tr >';
 
         } else {
-            results.innerHTML = '<tr><th scope="row">' + roundcounter + '</th><td>' + convertNumber(manDraw) + '</td><td>' + convertNumber(botDraw) + '</td><td>Wygrywa Komputer</td></tr >';
+            results.innerHTML = '<tr><th scope="row">' + params.roundcounter + '</th><td>' + translate(params.manDraw) + '</td><td>' + translate(params.botDraw) + '</td><td>Wygrywa Komputer</td></tr >';
 
         }
 
@@ -98,65 +99,51 @@ window.onload = function () {
 
 
     function totalResult() {
-        if (botWins < manWins) {
+        if (params.botWins < params.manWins) {
 
-            return 'Zwyciężył: ' + user + ' Stosunkeim ' + manWins + ' : ' + botWins + " Ilość remisów: " + remisy;
+            return 'Zwyciężył: ' + params.user + ' Stosunkeim ' + params.manWins + ' : ' + params.botWins + " Ilość remisów: " + params.remisy;
 
-        } else if (botWins > manWins) {
-            return ' Komputer wygrał Stosunkem: ' + botWins + ' : ' + manWins + " Ilość remisów: " + remisy;
+        } else if (params.botWins > params.manWins) {
+            return ' Komputer wygrał Stosunkem: ' + params.botWins + ' : ' + params.manWins + " Ilość remisów: " + params.remisy;
         } else {
-            return 'Nieźle był remis  Komputer: ' + botWins + ' wygranych, ' + user + ' ' + manWins + " wygranych  Ilość remisów: " + remisy;
+            return 'Nieźle był remis  Komputer: ' + params.botWins + ' wygranych, ' + params.user + ' ' + params.manWins + " wygranych  Ilość remisów: " + params.remisy;
         }
     }
 
     function totalRounds(x, b) {
         if (x < 5) {
-            console.log('wewnątrz total Rounds' + x);
             return;
         } else if (x == 5) {
-            console.log('wewnątrz total Rounds' + x);
             setTimeout(function () {
                 alert(totalResult());
             }, 500);
             results.innerHTML = '';
-
         }
-
         mainDiv1.classList.add('nonVisible');
         mainDiv1.classList.remove('Visible');
         mainDiv2.classList.add('Visible');
         mainDiv2.classList.remove('nonVisible');
     }
 
-    function play() {
-        botDraw = computerDraw();
-        var foo = whoWins(manDraw, botDraw);
+    function play(x) {
+        // console.log('z funkcji play ' + x);
+        params.botDraw = computerDraw();
+        var foo = whoWins(x, params.botDraw);
         updateResults();
-        roundcounter++;
-        callbackOutput(foo);
-        totalRounds(roundcounter, foo);
+        params.roundcounter++;
+        // console.log(params.roundcounter);
+        callbackOutput(foo, x);
+        totalRounds(params.roundcounter, foo);
     }
 
     var playerButtons = document.querySelectorAll('.player-move');
-    var userPlayCallback = function () {
-        console.log(playerButtons);
 
+    for (var i = 0; i < playerButtons.length; i++) {
+        playerButtons[i].addEventListener('click', function () {
+            params.manDraw = event.target.getAttribute('data-move');
+            play(params.manDraw);
+            return;
+        });
     }
-
-    scissors.addEventListener('click', function () {
-        manDraw = 2;
-        play();
-    });
-
-    rock.addEventListener('click', function () {
-        manDraw = 3;
-        play();
-    });
-    paper.addEventListener('click', function () {
-        manDraw = 1;
-        play();
-    });
-
-
 
 }
